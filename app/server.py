0a6c15c2dbc19142781462ae8e59aea984772b40
Abdulaziz Jaiz <abajall@gmail.com>
2020-04-04 14:38:38 +0300

    .

--- app/server.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import Flask,flash, redirect ,render_template , request, url_for
-
-app = Flask(__name__)
-
-@app.route("/")
-def list_file():
-    return ':)'
--- gunicorn.config.py
+++
@@ -1,8 +0,0 @@
-"""Gunicorn configuration."""
-
-bind = '127.0.0.1:5000'
-
-workers = 6
-worker_class = 'gevent'
-
-accesslog = '-'
